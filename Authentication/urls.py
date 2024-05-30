17f5ae00122767072cdb5571a81dea34d35a06fa
frwkHoangQuy <frwkHoangQuy@gmail.com>
2024-05-30 15:56:58 +0700

    [FEATURE] Thêm Auth Api

--- Authentication/urls.py
+++ Authentication/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from .views import RegisterView, LoginView, VerifyTokenView
 
-
 urlpatterns = [
     path('auth/login/', LoginView.as_view(), name="Login"),
     path('auth/register/', RegisterView.as_view(), name="Register"),
